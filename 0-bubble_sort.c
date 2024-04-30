9691d806516be6f96599cb03ed76ff7c389aa12e
Wongani <wonganichulu@gmail.com>
2024-04-30 10:59:09 +0200

    Added the bubble sort.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Function that does a bubble sort on an array.
+ * @array: Array to be sorted.
+ * @size: Size of the given array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i, swap, temp;
+
+	do {
+		swap = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (i != size - 1)
+			{
+				if (array[i] > array[i + 1])
+				{
+					temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					swap++;
+				}
+			}
+		}
+		print_array(array, size);
+	} while (swap != 0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the directory for the sorting alogorithm tasks for 0x1B. C - Sorting algorithms & Big O.
